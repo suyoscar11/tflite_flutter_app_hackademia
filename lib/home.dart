5e5cf8baaa7bdabb53354543f9b05a5c44139fa0
Suyoscar11 <suyoscar11@gmail.com>
2023-12-01 13:55:15 +0545

    flutter upgraded

--- lib/home.dart
+++ lib/home.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Finally, some Flutter'),
+      ),
+      body: Container(
+        child: Text('Container Datasets'),
+      ),
+    );
+  }
+}
