1f3c6e63fd9e943d4554d08d8f06adceccaa8362
Suyoscar11 <suyoscar11@gmail.com>
2023-12-02 08:59:24 +0545

    Chat's code...works but no texts

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,10 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
-import 'package:tflite_flutter_app_hackademia/main.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -15,36 +15,42 @@ class _MyHomePageState extends State<MyHomePage> {
   CameraController? cameraController;
   String output = '';
 
-  loadCamera() {
-    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
-    cameraController!.initialize().then((value) {
-      if (!mounted) {
-        return;
-      } else {
-        setState(() {
-          cameraController!.startImageStream((imageStream) {
-            cameraImage = imageStream;
-            runModel();
+  loadCamera() async {
+    var status = await Permission.camera.request();
+    if (status.isGranted) {
+      List<CameraDescription> cameras = await availableCameras();
+      cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+      cameraController!.initialize().then((value) {
+        if (!mounted) {
+          return;
+        } else {
+          setState(() {
+            cameraController!.startImageStream((imageStream) {
+              cameraImage = imageStream;
+              runModel();
+            });
           });
-        });
-      }
-    });
+        }
+      });
+    } else {
+      // Handle camera permission denial
+      print('Camera permission denied');
+    }
   }
 
   runModel() async {
     if (cameraImage != null) {
       var predictions = await Tflite.runModelOnFrame(
-          bytesList: cameraImage!.planes.map((plane) {
-            return plane.bytes;
-          }).toList(),
-          imageHeight: cameraImage!.height,
-          imageWidth: cameraImage!.width,
-          imageMean: 127.5,
-          imageStd: 127.5,
-          rotation: 90,
-          numResults: 2,
-          threshold: 0.1,
-          asynch: true);
+        bytesList: cameraImage!.planes.map((plane) => plane.bytes).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
 
       predictions!.forEach((element) {
         setState(() {
@@ -56,7 +62,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   loadModel() async {
     await Tflite.loadModel(
-        model: 'assets/model_unquant.tflite', labels: 'assets/label.txt');
+      model: 'assets/model_unquant.tflite',
+      labels: 'assets/label.txt',
+    );
   }
 
   @override
@@ -67,29 +75,37 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void dispose() {
+    cameraController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Finally, some Flutter'),
-        ),
-        body: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(20)),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.7,
-              width: MediaQuery.of(context).size.width,
-              child: !cameraController!.value.isInitialized
-                  ? Container()
-                  : AspectRatio(
-                      aspectRatio: cameraController!.value.aspectRatio,
-                      child: CameraPreview(cameraController!),
-                    ),
-            ),
-            Text(
-              output,
-              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            )
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Flutter Camera and TFLite'),
+      ),
+      body: Column(
+        children: [
+          const Padding(padding: EdgeInsets.all(20)),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width,
+            child: !cameraController!.value.isInitialized
+                ? Container()
+                : AspectRatio(
+                    aspectRatio: cameraController!.value.aspectRatio,
+                    child: CameraPreview(cameraController!),
+                  ),
+          ),
+          Text(
+            output,
+            style: const TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+        ],
+      ),
+    );
   }
 }
